215658f86c4be0de7e481f1103b59fd7bdc4606a
brandon14 <b_randon14@hotmail.com>
2011-11-16 05:16:12 -0500

    initial commit: initial push of bkernel_cm7 source tree
    this is the first push of source code to the bkernel_cm7 source
    it picks up at bKernel-CM7-DEV-0.3

---
+++ build.sh
@@ -0,0 +1,43 @@
+###################################################
+#build.sh -                                       #
+#Shell script to build the froyo kernel for the   #
+#Motorola Triumph and zip it into a CWM flashable #
+#package                                          #
+#                                                 #
+#Written by shane87                               #
+#Modified by b_randon14                           #
+###################################################
+
+#This part exports the variables needed by the build script in order to build the kernel correctly
+export ARCH=arm
+#This is the kernel version that will be appended to the default kernel version
+export KBUILD_BUILD_VERSION="bKernel-CM7-0.3-DEV"
+#This is the name that the CWM flashable zip will be named as, MUST END WITH .zip!!
+KERNELZIP_VERSION=bKernel-CM7-0.3-DEV.zip
+
+#This exports the path to you toolchain, it is set up to use the prebuilt CM7 toolchian, in a custom directory
+#If you intend to use another toolchain please update this variable to reflect where the new toolchain is located at
+export CROSS_COMPILE=/home/`whoami`/android-toolchain-eabi/bin/arm-eabi-
+
+#This section cleans out the build directory to ensure a good build
+make clean
+
+#This section initiates the build process, and it is optimized to read the processor infor of your system and build as many things 
+#as it can at one time to speed the make process up
+make -j`grep 'processor' /proc/cpuinfo | wc -l`
+
+#This next section is where it take the zImage(compressed kernel image) and combines it with the init ramdisk created and packages it as a boot.img
+#and then puts it into the flashable zip file.
+cd releasetools
+rm -f *.img
+cp ../arch/arm/boot/zImage zImage
+../tools/bootimg/mkbootfs ../usr/initram-files | gzip > ramdisk.gz
+../tools/bootimg/mkbootimg --kernel zImage --ramdisk ramdisk.gz --cmdline "console=ttyMSM1 androidboot.hardware=qcom" -o boot.img --base 0x00200000
+rm -f zImage
+rm -f *.gz
+rm -f *.zip
+zip -r $KERNELZIP_VERSION *
+cd ..
+echo "Finished building bKernel. Please flash the zip through CWM to test it!"
+echo "Always remember to make a nandroid backup!"
+unset ARCH
