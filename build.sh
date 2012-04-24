33c62273c62767d0117280f027c0f998c716611d
brandon14 <b_randon14@hotmail.com>
2012-04-24 11:28:15 -0400

    toolchain: removing toolchain from directory

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@
 #This part exports the variables needed by the build script in order to build the kernel correctly
 export ARCH=arm
 #This is the kernel version that will be appended to the default kernel version
-export KBUILD_BUILD_VERSION="bKernel-CM7-0.7-DEV"
+export KBUILD_BUILD_VERSION="bKernel-CM7-0.9-DEV"
 #This is the name that the CWM flashable zip will be named as, MUST END WITH .zip!!
-KERNELZIP_VERSION=bKernel-CM7-0.7-DEV.zip
+KERNELZIP_VERSION=bKernel-CM7-0.9-DEV.zip
 
 #This exports the path to you toolchain, and it currently setup for the linaro 4.6 android toolchain found at www.linaro.org that I have included in the repo
 export CROSS_COMPILE=/home/`whoami`/android-toolchain-eabi/bin/arm-eabi-
