b9321518a2673d206b05ead2c3551dcb1e91e95f
brandon14 <b_randon14@hotmail.com>
2012-04-29 06:49:46 -0400

    Linux patch 2.6.32.9.11

--- lib/lcm.c
+++ lib/lcm.c
@@ -5,11 +5,11 @@
 /* Lowest common multiple */
 unsigned long lcm(unsigned long a, unsigned long b)
 {
-if (a && b)
-return (a * b) / gcd(a, b);
-else if (b)
-return b;
+	if (a && b)
+		return (a * b) / gcd(a, b);
+	else if (b)
+		return b;
 
-return a;
+	return a;
 }
 EXPORT_SYMBOL_GPL(lcm);
