68c224c0f9b356824f3e7bc06ad80d7d5c2be6d5
brandon14 <b_randon14@hotmail.com>
2012-05-04 06:08:33 -0400

    config: enable FastFPE, disable some FIH debugging, update bKernel to v1.0 update gitignore

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@
 #This part exports the variables needed by the build script in order to build the kernel correctly
 export ARCH=arm
 #This is the kernel version that will be appended to the default kernel version
-export KBUILD_BUILD_VERSION="bKernel-CM7-0.9-DEV"
+export KBUILD_BUILD_VERSION="bKernel-CM7-1.0"
 #This is the name that the CWM flashable zip will be named as, MUST END WITH .zip!!
-KERNELZIP_VERSION=bKernel-CM7-0.9-DEV.zip
+KERNELZIP_VERSION=bKernel-CM7-1.0.zip
 
-#This exports the path to you toolchain, and it currently setup for the linaro 4.6 android toolchain found at www.linaro.org that I have included in the repo
+#This exports the path to you toolchain, and it currently setup for the linaro 4.6 android toolchain found at www.linaro.org
 export CROSS_COMPILE=/home/`whoami`/android-toolchain-eabi/bin/arm-eabi-
 
 #This section cleans out the build directory to ensure a good build
